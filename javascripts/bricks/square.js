b7e9530edf26f22f57e94591c2f5cb2342c7d0da
Mathias Paumgarten <mail@mathias-paumgarten.com>
2011-07-10 08:44:03 -0400

    adds class for square brick

---
+++ javascripts/bricks/square.js
@@ -0,0 +1,24 @@
+var Square = function() {
+  this.row = 0;
+  this.column = 0;
+}
+
+Square.prototype.draw = function(context) {
+	
+  context.save();
+	
+	context.translate(this.column * brickSize, this.row * brickSize);
+  context.beginPath();
+	
+  context.fillColor = 0;
+  context.moveTo(0, 0);
+  context.lineTo(brickSize, 0);
+  context.lineTo(brickSize, brickSize);
+  context.lineTo(0, brickSize);
+	
+  context.closePath();
+	
+  context.fill();
+	
+  context.restore();
+}
